92e7c115bb78e5a2887cbf0b0e4a4168c93512f9
Ortal Yatzkan <ortalyatzkan@gmail.com>
2022-04-10 00:38:29 +0300

    Renaming variables

--- perfect_dish_to_division.py
+++ perfect_dish_to_division.py
@@ -1,13 +1,13 @@
-def dividers_equal_to_sum_generator(start=2)->int:
+def dividers_equal_to_sum_generator(size_of_dish=2)->int:
     """
     A function of a generator that calculates the numbers whose sum of divisors is equal to the number itself.
-    :param start:The number from which The generator function starts calculating.
+    :param size_of_dish:The number from which The generator function starts calculating.
     :return:Perfect number for division.
     """
     while True:
-        sum_dividers = sum([num for num in range(1, start) if start % num == 0])
-        (yield sum_dividers) if sum_dividers == start else None
-        start += 1
+        sum_dividers = sum([divider for divider in range(1, size_of_dish) if size_of_dish % divider == 0])
+        (yield sum_dividers) if sum_dividers == size_of_dish else None
+        size_of_dish += 1
 
 
 if __name__== '__main__':
