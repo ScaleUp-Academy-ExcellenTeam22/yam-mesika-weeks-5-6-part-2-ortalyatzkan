f9f45d8a091fa5a63ee9821e802d98244b066242
Ortal Yatzkan <ortalyatzkan@gmail.com>
2022-04-10 00:29:21 +0300

    Adding spaces and renaming variables

--- connected_tools.py
+++ connected_tools.py
@@ -2,13 +2,14 @@ import functools
 import itertools
 
 
-def interleave(*iterables):
+def interleave(*iterables)->list:
     """
     A function that connects lists in the order of the values.
     :param iterables:A Tuple of list to merge.
     :return:A merge list of the  lists she get by the order of the values.
     """
-    return list(functools.reduce(lambda x, y: x + y, [iterable for iterable in itertools.zip_longest(*iterables)]))
+    return list(functools.reduce(lambda first_item, second_item: first_item + second_item, [iterable for iterable in itertools.zip_longest(*iterables)]))
+
 
 def interleave_generator(*iterables)->list:
     """
@@ -16,13 +17,14 @@ def interleave_generator(*iterables)->list:
     :param iterables:A Tuple of list to merge.
     :return:A merge list of the  lists she get by the order of the values.
     """
-    x=[]
+    save_interleave_list=[]
     for iterable in itertools.zip_longest(*iterables):
-        x += interleave(iterable)
-        yield x
+        save_interleave_list += interleave(iterable)
+        yield save_interleave_list
+
 
 if __name__== '__main__':
     print(interleave('abc', [1, 2, 3], ('!', '@', '#')))
-    interleave_f = interleave_generator('abc', [1, 2, 3], ('!', '@', '#'))
-    for i in  interleave_f:
-        print(i)
+    interleave_lists = interleave_generator('abc', [1, 2, 3], ('!', '@', '#'))
+    for interleave_list in  interleave_lists:
+        print(interleave_list)
